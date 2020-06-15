4ec3f2e45b7eabff0dbd6c72e692f621b1806ed8
Ysaakue <isaacgondim82@gmail.com>
2020-06-15 00:15:18 -0300

    pre configurations to sent emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@
 class User < ActiveRecord::Base
   extend Devise::Models
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,:registerable,:confirmable,
+        :recoverable,:rememberable,:trackable,:validatable
   include DeviseTokenAuth::Concerns::User
 end
